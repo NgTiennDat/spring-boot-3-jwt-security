8bfa6e251ed623cb79f8a6d39e217c2d66297db8
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-07-27 23:17:38 +0700

    add permission

--- src/main/java/com/datien/jwtsecurity/user/Role.java
+++ src/main/java/com/datien/jwtsecurity/user/Role.java
@@ -1,6 +1,49 @@
 package com.datien.jwtsecurity.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.datien.jwtsecurity.user.Permission.*;
+
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    USER,
-    ADMIN
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE,
+                    MANAGER_CREATE
+            )
+    ),
+    MANAGER(
+            Set.of(
+                    MANAGER_READ,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE,
+                    MANAGER_CREATE
+            )
+    );
+
+    private final Set<Permission> permissions;
+
+    private List<SimpleGrantedAuthority> grantedAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    };
 }
