e1c38d3feb0eec6a9d0bd3e8392ed751d6f88d93
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-08-01 09:45:37 +0700

    add attribute for password

--- src/main/java/com/datien/jwtsecurity/user/model/UserForgotPassword.java
+++ src/main/java/com/datien/jwtsecurity/user/model/UserForgotPassword.java
@@ -1,4 +1,6 @@
 package com.datien.jwtsecurity.user.model;
 
 public class UserForgotPassword {
+
+    private String email;
 }
--- src/main/java/com/datien/jwtsecurity/user/model/UserResetPassword.java
+++ src/main/java/com/datien/jwtsecurity/user/model/UserResetPassword.java
@@ -1,4 +1,8 @@
 package com.datien.jwtsecurity.user.model;
 
 public class UserResetPassword {
+    private String email;
+
+    private String newPassword;
+    private String confirmPassword;
 }
